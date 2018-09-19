5f2ff5eaf69c390e32620c9d7f63a638803e699a
Maico Ribeiro <maico.unilever@gmail.com>
2018-09-19 15:10:09 -0300

    Updated sdk versions to 26.1.0

---
+++ app/src/main/java/com/redside/rngquest/managers/CutsceneManager.java
@@ -0,0 +1,10 @@
+package com.redside.rngquest.managers;
+
+/**
+ * Manages other managers.
+ * The long chain of ticks and renders start here.
+ * @author Maico Ribeiro
+ * @since September 19, 2018
+ */
+public class CutsceneManager {
+}
